2db14b5db76f225e8f6be28c6dbc9eda006cc875
L4igi <l.hoewarth@gmail.com>
2020-11-28 23:04:04 +0100

    Character Stats
    Added diffetent states depending on character state. Ground Air etc. Made these staes more modular in code

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -12,6 +12,7 @@ var dropDownCount = 0
 var snapEdgePosition = Vector2()
 var disableInput = false
 var shortHop = true
+var availabelJumps = 2
 
 var directionChange = false
 
@@ -20,14 +21,24 @@ var velocity = Vector2()
 onready var gravity = 2000
 onready var baseGravity = gravity
 
+enum CharacterState{GROUND, AIR, EDGE, ATTACK, ROLL, STUN}
+
+var currentState = CharacterState.GROUND
+
 func _physics_process(delta):
+	print(currentState)
 	if !disableInput:
-		if !snapEdge:
-			basic_movement(delta)
+		if currentState == CharacterState.EDGE:
+			edge_handler(delta)
+		elif currentState == CharacterState.AIR:
+			air_handler(delta)
 		else:
-			snap_edge_input(delta)
+			basic_movement(delta)
 	
 func basic_movement(delta):
+	#reset gravity if player is grounded
+	if gravity!=baseGravity:
+		gravity=baseGravity
 	# Horizontal movement code. First, get the player's input.
 	var walk = WALK_FORCE * (Input.get_action_strength("right") - Input.get_action_strength("left"))
 	# Slow down the player if they're not trying to move.
@@ -49,36 +60,17 @@ func basic_movement(delta):
 
 	# Move based on the velocity and snap to the ground.
 	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
-
-	#reset gravity if player is jumping
-	if Input.is_action_just_pressed("jump") && gravity != baseGravity: 
-		gravity = baseGravity
 	# Check for jumping. is_on_floor() must be called after movement code
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = -JUMP_SPEED
-		jumpCount += 1
-		create_jump_timer(0.15)
-	elif !is_on_floor() and Input.is_action_just_pressed("jump") and jumpCount == 1:
-		velocity.y = -JUMP_SPEED
-		jumpCount += 1
-	#rests jump count when player lands back on the ground
-	elif is_on_floor() and jumpCount > 0: 
-		jumpCount = 0
-	elif !is_on_floor() and jumpCount == 0: 
-		jumpCount = 1
-	#reset gravity
-	elif is_on_floor() && gravity != baseGravity:
-		gravity = baseGravity
-	#shorthop depending on button press length 
-	if Input.is_action_just_released("jump") && jumpCount == 1: 
-		print("ShortHop")
-		shortHop = true
-	
-	#enable collisons with platforms if player is falling down
-	if !collidePlatforms && !is_on_floor() && velocity.y >= 0: 
+	if Input.is_action_just_pressed("jump"):
+		#reset gravity if player is jumping
 		collidePlatforms = true
 		set_collision_mask_bit(1,true)
-
+		dropDownCount = 0
+		velocity.y = -JUMP_SPEED
+		jumpCount = 1
+		create_jump_timer(0.15)
+		currentState = CharacterState.AIR
+	#shorthop depending on button press length 
 	if Input.is_action_just_pressed("down"):
 		dropDownCount += 1
 		for i in get_slide_count():
@@ -89,15 +81,56 @@ func basic_movement(delta):
 				create_drop_platform_timer(0.3, false)
 			else: 
 				create_drop_platform_timer(0.5, true)
+	#checks if player walked off platform/stage
+	if velocity.y != 0:
+		jumpCount = 1
+		currentState = CharacterState.AIR
+		
+#is called when player is in the air 
+func air_handler(delta):
+	var walk = WALK_FORCE * (Input.get_action_strength("right") - Input.get_action_strength("left"))
+	# Slow down the player if they're not trying to move.
+	if abs(walk) < WALK_FORCE * 0.2:
+		# The velocity, slowed down a bit, and then reassigned.
+		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+	else:
+		if velocity.x >= 0 && walk < 0 || velocity.x <= 0 && walk > 0: 
+			directionChange = true
+			velocity.x += walk*3 * delta
+		else: 
+			directionChange = false
+			velocity.x += walk * delta
+	# Clamp to the maximum horizontal movement speed.
+	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
+
+	# Vertical movement code. Apply gravity.
+	velocity.y += gravity * delta
+
+	# Move based on the velocity and snap to the ground.
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	if Input.is_action_just_pressed("jump") && jumpCount < availabelJumps:
+		if gravity!=baseGravity:
+			gravity=baseGravity
+		velocity.y = -JUMP_SPEED
+		jumpCount += 1
+	if Input.is_action_just_released("jump") && jumpCount == 1:
+		shortHop = true
+	#enable collisons with platforms if player is falling down
+	if !collidePlatforms && velocity.y >= 0: 
+		collidePlatforms = true
+		set_collision_mask_bit(1,true)
 	#Fastfall
 	if Input.is_action_just_pressed("down") && !is_on_floor() && velocity.y > 0:
 		gravity = 4000
-				
+	if is_on_floor():
+		currentState = CharacterState.GROUND
+	
 func _on_short_hop_timeout():
 	if shortHop: 
 		velocity.y=0
 				
 func _on_drop_platform_timeout():
+	currentState = CharacterState.AIR
 	collidePlatforms = false
 
 func _on_drop_input_timeout():
@@ -126,6 +159,7 @@ func create_drop_platform_timer(waittime,inputTimeout):
  
 func snap_edge(edgePosition):
 	if !is_on_floor():
+		currentState = CharacterState.EDGE
 		disableInput = true
 		snapEdgePosition = edgePosition
 		velocity = Vector2.ZERO
@@ -138,12 +172,16 @@ func snap_edge(edgePosition):
 		snapEdge = true
 		disableInput = false
 
-func snap_edge_input(delta):
+func edge_handler(delta):
+	disableInput = true
 	jumpCount = 0
 	velocity = Vector2.ZERO
+	var targetPosition = Vector2.ZERO
 	if Input.is_action_just_pressed("down"):
+		currentState = CharacterState.AIR
 		snapEdge=false
 	elif Input.is_action_just_pressed("jump"):
+		currentState = CharacterState.AIR
 		velocity.y = -JUMP_SPEED
 		jumpCount += 1
 		#disables collisons with platforms if player is jumping upwards
@@ -152,16 +190,61 @@ func snap_edge_input(delta):
 		snapEdge=false
 	elif Input.is_action_just_pressed("left"):
 		if global_position < snapEdgePosition:
+			currentState = CharacterState.AIR
 			velocity.x = -WALK_MAX_SPEED/4
+			snapEdge=false
 		else:
-			pass
-		snapEdge=false
+			targetPosition = snapEdgePosition - get_character_size()/2
+			$Tween.interpolate_property(self, "position", global_position, targetPosition , 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+			$Tween.start()
+			yield($Tween, "tween_all_completed")
+			snapEdge=false
+			currentState = CharacterState.GROUND
 	elif Input.is_action_just_pressed("right"):
 		if global_position > snapEdgePosition:
 			velocity.x = WALK_MAX_SPEED/4
+			snapEdge=false
+			currentState = CharacterState.AIR
 		else: 
-			pass
-		snapEdge=false
+			targetPosition = snapEdgePosition - Vector2(-(get_character_size()/2).x,(get_character_size()/2).y)
+			$Tween.interpolate_property(self, "position", global_position, targetPosition , 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+			$Tween.start()
+			yield($Tween, "tween_all_completed")
+			snapEdge=false
+			currentState = CharacterState.GROUND
+	elif Input.is_action_just_pressed("up"):
+		if global_position > snapEdgePosition:
+			#normal getup right edge
+			targetPosition = snapEdgePosition - get_character_size()/2
+			$Tween.interpolate_property(self, "position", global_position, targetPosition , 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+			$Tween.start()
+			yield($Tween, "tween_all_completed")
+			snapEdge=false 
+			currentState = CharacterState.GROUND
+		else: 
+			targetPosition = snapEdgePosition - Vector2(-(get_character_size()/2).x,(get_character_size()/2).y)
+			$Tween.interpolate_property(self, "position", global_position, targetPosition , 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+			$Tween.start()
+			yield($Tween, "tween_all_completed")
+			snapEdge=false 
+			currentState = CharacterState.GROUND
+	elif Input.is_action_just_pressed("shield"):
+		if global_position > snapEdgePosition:
+			#normal getup right edge
+			targetPosition = snapEdgePosition - Vector2((get_character_size()/2).x*4,(get_character_size()/2).y)
+			$Tween.interpolate_property(self, "position", global_position, targetPosition , 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+			$Tween.start()
+			yield($Tween, "tween_all_completed")
+			snapEdge=false 
+			currentState = CharacterState.GROUND
+		else: 
+			targetPosition = snapEdgePosition - Vector2(-(get_character_size()/2).x*4,(get_character_size()/2).y)
+			$Tween.interpolate_property(self, "position", global_position, targetPosition , 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+			$Tween.start()
+			yield($Tween, "tween_all_completed")
+			snapEdge=false 
+			currentState = CharacterState.GROUND
+	disableInput = false
 
 func get_character_size():
 	return $Sprite.texture.get_size()
