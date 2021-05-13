a4a00da9f7a1e0dab56beb34c60ca664ca9236b9
L4igi <l.hoewarth@gmail.com>
2021-05-13 23:07:39 +0200

    Character GUI Node Done

--- UI/CharacterContainer.gd
+++ UI/CharacterContainer.gd
@@ -1,24 +1,30 @@
 extends MarginContainer
 
-onready var characterPortrait = get_node("VBoxContainer/HBoxContainer/CharacterPortrait")
+onready var characterPortrait = get_node("VBoxContainer/HBoxContainer/MarginContainer/CharacterPortrait")
 onready var characterNameLabel = get_node("VBoxContainer/HBoxContainer/VBoxContainer/CharacterName/CharacterNameLabel")
 onready var characterNameBg = get_node("VBoxContainer/HBoxContainer/VBoxContainer/CharacterName/CharacterNameBg")
-onready var stocksFilles = get_node("VBoxContainer/StocksContainer/StocksFilled")
-onready var stocksEmpty = get_node("VBoxContainer/StocksContainer/stocksEmpty")
+onready var stocksContainer = get_node("VBoxContainer/StocksContainer/HBoxContainer")
+onready var damagePercentLabel = get_node("VBoxContainer/HBoxContainer/VBoxContainer/MarginContainer/DamagePercent")
 
+onready var stocksTextureRect = preload("res://UI/StocksTextureRect.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func set_label_name(charName):
-	pass
+	characterNameLabel.set_text(charName)
 	
 func set_character_portrait(portraittexture):
-	pass
+	characterPortrait.set_texture(portraittexture)
 	
 func set_damage_percent(damagePercent):
-	pass
+	damagePercentLabel.set_text(str(damagePercent) + "%")
 	
 func set_stocks(stocks):
-	pass
+	for i in stocks:
+		var newStock = stocksTextureRect.instance()
+		stocksContainer.add_child(newStock)
 	
+func remove_stock():
+	var allStocks = stocksContainer.get_children()
+	allStocks.back().queue_free()
